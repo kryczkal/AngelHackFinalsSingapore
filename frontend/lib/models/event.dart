f88d2227ae627de3348943648ac64a2339a10735
Kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2024-10-19 15:47:18 +0200

    mockup data

--- frontend/lib/models/event.dart
+++ frontend/lib/models/event.dart
@@ -1,13 +1,26 @@
+import 'package:frontend/models/EventBadge.dart';
+import 'package:frontend/models/user.dart';
+
 class Event {
   final String title;
   final String date;
   final String imageUrl;
   final String backgroundColor;
+  final String localization;
+  final String description;
+  final User organizer;
+  List<User> registeredUsers;
+  List<EventBadge> badges;
 
   Event({
     required this.title,
     required this.date,
     required this.imageUrl,
     required this.backgroundColor,
+    required this.localization,
+    required this.description,
+    required this.organizer,
+    required this.registeredUsers,
+    required this.badges,
   });
 }
