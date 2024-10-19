1fbc85dbe198e8f1b017fbbf9326d3e0040a2cc9
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-10-19 17:19:26 +0200

    Added models

---
+++ frontend/lib/models/event.dart
@@ -0,0 +1,13 @@
+class Event {
+  final String title;
+  final String date;
+  final String imageUrl;
+  final String backgroundColor;
+
+  Event({
+    required this.title,
+    required this.date,
+    required this.imageUrl,
+    required this.backgroundColor,
+  });
+}
