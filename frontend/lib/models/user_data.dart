2063f168f63252cac4d09d1249ef47d22805153e
Kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2024-10-23 20:46:10 +0200

    create event page

--- frontend/lib/models/user_data.dart
+++ frontend/lib/models/user_data.dart
@@ -1,13 +1,17 @@
 import 'package:frontend/models/event_badge.dart';
 import 'package:frontend/models/event_categories_enum.dart';
+import 'package:frontend/models/event_data.dart';
+import 'package:frontend/models/lyf_hotels_enum.dart';
 
 class User {
   final String firstName;
   final String lastName;
   final int age;
   final String profilePic = 'assets/default_profile_pic.png';
+  final LyfHotels hotel = LyfHotels.Funan;
   List<EventBadge> userBadges;
   Set<EventCategory> preferences;
+  List<Event> createdEvents = [];
 
   User({
     required this.firstName,
