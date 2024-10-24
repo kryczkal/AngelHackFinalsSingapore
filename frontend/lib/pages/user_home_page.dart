372696d6e99a0a01dbdd7b168c7ad95afb1676b0
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-10-24 16:39:36 +0200

    More refactoring

--- frontend/lib/pages/user_home_page.dart
+++ frontend/lib/pages/user_home_page.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/app_data/app_events.dart';
 import 'package:frontend/models/event_data.dart';
-import 'package:frontend/pages/events/joined_created_events_page.dart';
-import 'package:frontend/pages/events/create_event_page.dart';
 import 'package:frontend/widgets/events/event_card.dart';
+import 'package:frontend/widgets/events/events_dashboard_header.dart';
 import 'package:frontend/widgets/events/events_header.dart';
 import 'package:frontend/widgets/misc/app_scroll_behavior_web_extended.dart';
 import 'package:frontend/widgets/misc/ignore_padding_widget.dart';
-import 'package:frontend/widgets/events/joined_events_card.dart';
 import 'package:frontend/widgets/user/user_profile_header.dart';
-import 'package:frontend/widgets/events/your_events_card.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
@@ -34,52 +31,10 @@ class UserPage extends StatelessWidget {
                 children: [
                   const SizedBox(height: 37),
                   const ProfileHeader(hasNotification: true),
-                  const SizedBox(height: 37),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const JoinedEventsPage()),
-                            );
-                          },
-                          child: const SizedBox(
-                            height: 176,
-                            child: JoinedEventsCard(),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 24),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const CreateEventPage()),
-                            );
-                          },
-                          child: Column(
-                            children: [
-                              SizedBox(height: 46),
-                              SizedBox(
-                                height: 176,
-                                child: YourEventsCard(),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  const SizedBox(height: 12),
+                  const EventsDashboardHeader(),
                   const SizedBox(height: 24),
-                  const EventsHeader(title: 'Events on\nThis month'),
+                  const EventsHeader(title: 'Might\ninterest you'),
                   const SizedBox(height: 16),
                   IgnorePaddingWidget(
                     child: SizedBox(
@@ -96,8 +51,27 @@ class UserPage extends StatelessWidget {
                       ),
                     ),
                   ),
+                  const SizedBox(height: 32),
+                  const EventsHeader(title: 'Last\nMinute'),
                   const SizedBox(height: 16),
-                  const EventsHeader(title: 'Events\nAround you'),
+                  IgnorePaddingWidget(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 250,
+                      child: PageView.builder(
+                        scrollBehavior: ScrollBehaviorWebExtended(),
+                        controller: PageController(viewportFraction: 0.80),
+                        itemCount: AppEventsSingleton().events.length,
+                        itemBuilder: (context, index) {
+                          return _buildEventCard(
+                              AppEventsSingleton().events[index]);
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  const EventsHeader(title: 'Best\nglobally'),
+                  const SizedBox(height: 16),
                   IgnorePaddingWidget(
                     child: SizedBox(
                       width: MediaQuery.of(context).size.width,
---
+++ frontend/lib/widgets/events/events_dashboard_header.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/events/create_event_page.dart';
+import 'package:frontend/pages/events/joined_created_events_page.dart';
+import 'package:frontend/widgets/events/joined_events_card.dart';
+import 'package:frontend/widgets/events/your_events_card.dart';
+
+class EventsDashboardHeader extends StatelessWidget {
+  const EventsDashboardHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const JoinedEventsPage()),
+              );
+            },
+            child: const SizedBox(
+              height: 176,
+              child: JoinedEventsCard(),
+            ),
+          ),
+        ),
+        const SizedBox(width: 24),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const CreateEventPage()),
+              );
+            },
+            child: Column(
+              children: [
+                SizedBox(height: 46),
+                SizedBox(
+                  height: 176,
+                  child: YourEventsCard(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
