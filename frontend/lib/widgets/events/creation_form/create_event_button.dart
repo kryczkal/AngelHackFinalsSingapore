5a422927efce27adbc902f8a9fdec61c7b1b554d
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-10-24 22:11:23 +0200

    Updated font in event create button

--- frontend/lib/widgets/events/creation_form/create_event_button.dart
+++ frontend/lib/widgets/events/creation_form/create_event_button.dart
@@ -96,7 +96,6 @@ class CreateEventButton extends StatelessWidget {
             style: TextStyle(
               fontSize: 16.0,
               color: Colors.white,
-              fontWeight: FontWeight.bold,
             ),
           ),
         ),
